ecb560e4acffbea9d34195b29f89126f79dfb4ee
Jimmy <nkgogojimmy@gmail.com>
2025-03-22 18:15:57 +0200

    login works.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
   const { fullName, email, password } = req.body;
@@ -25,14 +26,57 @@ export const signup = async (req, res) => {
 
     const newUser = new User({ fullName, email, password: hashedPassword });
     if (newUser) {
-        // generate jwt
+      // generate jwt
+      generateToken(newUser._id, res);
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid user data." });
     }
-    else {
-        return res.status(400).json({ message: "Invalid user data."})
-    }
-  } catch (error) {}
+  } catch (error) {
+    console.log("Error in signup controller", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 };
 
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Email and Password fields are required!" });
+    }
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(404).json({ message: "Invalid credentials." });
+    }
+
+    const passwordMatches = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatches) {
+      return res.status(404).json({ message: "Invalid credentials." });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.log("Error in login controller");
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
 
 export const logout = async (req, res) => {};
--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,17 +1,16 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId, res) => {
-
   const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: "7d",
   });
 
-  res.cookie("jwt",token, {
+  res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 1000, //7 days in milliseconds
     httpOnly: true, // this prevents cross-site scriptiong
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development"
-  })
+    secure: process.env.NODE_ENV !== "development",
+  });
 
   return token;
 };
