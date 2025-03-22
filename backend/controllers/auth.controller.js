260a24441953bb74e40e4acb19412aee98c3c09d
Jimmy <nkgogojimmy@gmail.com>
2025-03-22 17:23:49 +0200

    created the generateToken

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,14 +1,38 @@
-import bcrypt from "bcryptjs"
-
+import bcrypt from "bcryptjs";
+import User from "../models/user.model.js";
 
 export const signup = async (req, res) => {
-    const { name, email, password } = req.param;
-}
+  const { fullName, email, password } = req.body;
+  try {
+    if (!fullName || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Name, email and password are all required." });
+    } else if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "Password must be atleast 6 characters long." });
+    }
 
-export const login = async (req,res ) => {
+    const user = await User.findOne({ email });
 
-}
+    if (user) {
+      return res.status(400).json({ message: "Email already exists." });
+    }
 
-export const logout = async (req, res) => {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
 
-}
+    const newUser = new User({ fullName, email, password: hashedPassword });
+    if (newUser) {
+        // generate jwt
+    }
+    else {
+        return res.status(400).json({ message: "Invalid user data."})
+    }
+  } catch (error) {}
+};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 import express from "express";
-import authRoutes from "./routes/auth.route.js"
+import authRoutes from "./routes/auth.route.js";
 import { config } from "dotenv";
 import { connectDB } from "./lib/db.js";
 
 config();
 
 const app = express();
-
+app.use(express.json());
 const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log("App running at port: " + PORT);
-    connectDB();
-})
+  console.log("App running at port: " + PORT);
+  connectDB();
+});
