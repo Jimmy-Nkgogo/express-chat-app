0b71dbde0778b9f4df371cb1b8c1679230cfac37
Jimmy <nkgogojimmy@gmail.com>
2025-03-23 04:09:52 +0200

    started working on messages controllers

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,8 @@ import cookieParser from "cookie-parser";
 import { config } from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
 
 config();
@@ -15,6 +17,7 @@ app.use(cookieParser());
 const PORT = process.env.PORT;
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
   console.log("App running at port: " + PORT);
