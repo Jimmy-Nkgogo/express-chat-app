a3af50c6760c65ac6df8eaf11ae668cd3a636b13
Jimmy <nkgogojimmy@gmail.com>
2025-03-23 04:44:25 +0200

    done with the messages controllers.

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import Message from "../models/message.model.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const getUsersForSidebar = async (req, res) => {
   try {
@@ -28,5 +29,35 @@ export const getMessages = async (req, res) => {
   } catch (error) {
     console.log("Error in getMessages controller:", error);
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadedResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadedResponse.secure_url;
+    }
+
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    newMessage.save();
+
+    // TODO: realtime functionality goes here
+    res.status(200).json({ message: "Message sent successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error in sendMessage controller:", error });
   }
 };
