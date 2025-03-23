c1738e2f4574b97bed526e4684ea4a6d7cb0c66b
Jimmy <nkgogojimmy@gmail.com>
2025-03-23 03:02:21 +0200

    logout controller works

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -79,4 +79,11 @@ export const login = async (req, res) => {
   }
 };
 
-export const logout = async (req, res) => {};
+export const logout = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 0 });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
